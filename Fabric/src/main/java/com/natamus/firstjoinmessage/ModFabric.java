d64ab7aa591015e18c13fed8fc6d32e26e0976dc
Rick South <rick@natamus.com>
2023-12-12 10:14:34 +0100

    First Join Message source code for 1.20.4.

---
+++ Fabric/src/main/java/com/natamus/firstjoinmessage/ModFabric.java
@@ -0,0 +1,32 @@
+package com.natamus.firstjoinmessage;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.firstjoinmessage.events.FirstSpawnEvent;
+import com.natamus.firstjoinmessage.util.Reference;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+
+public class ModFabric implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		setGlobalConstants();
+		ModCommon.init();
+
+		loadEvents();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadEvents() {
+		ServerEntityEvents.ENTITY_LOAD.register((Entity entity, ServerLevel world) -> {
+			FirstSpawnEvent.onSpawn(world, entity);
+		});
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
