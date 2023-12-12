831b1876977eab848f34f4b7a59606e88b33f06f
Rick South <rick@natamus.com>
2023-12-12 10:14:25 +0100

    First Join Message source code for 1.20.2.

---
+++ NeoForge/src/main/java/com/natamus/firstjoinmessage/neoforge/events/NeoForgeFirstSpawnEvent.java
@@ -0,0 +1,14 @@
+package com.natamus.firstjoinmessage.neoforge.events;
+
+import com.natamus.firstjoinmessage.events.FirstSpawnEvent;
+import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeFirstSpawnEvent {
+	@SubscribeEvent
+	public static void onSpawn(EntityJoinLevelEvent e) {
+		FirstSpawnEvent.onSpawn(e.getLevel(), e.getEntity());
+	}
+}
