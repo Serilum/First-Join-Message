afd7badac8b644c1fdf69519d3877a48cc817c66
Rick South <rick@natamus.com>
2024-03-09 13:55:39 +0100

    (v3.5) Updated the mod to use the Collective's improved MessageFunctions instead of StringFunctions.

--- Common/src/main/java/com/natamus/firstjoinmessage/util/Reference.java
+++ Common/src/main/java/com/natamus/firstjoinmessage/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.firstjoinmessage.util;
 public class Reference {
 	public static final String MOD_ID = "firstjoinmessage";
 	public static final String NAME = "First Join Message";
-	public static final String VERSION = "3.4";
+	public static final String VERSION = "3.5";
 	public static final String ACCEPTED_VERSIONS = "[1.20.4]";
 }
