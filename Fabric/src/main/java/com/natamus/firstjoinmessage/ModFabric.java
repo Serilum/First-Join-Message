8d125f7327671ba7d9f3bf0897cbecb52b2509fc
Rick South <rick@natamus.com>
2024-07-28 07:33:14 +0200

    (v3.6) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Fabric/src/main/java/com/natamus/firstjoinmessage/ModFabric.java
+++ Fabric/src/main/java/com/natamus/firstjoinmessage/ModFabric.java
@@ -1,6 +1,7 @@
 package com.natamus.firstjoinmessage;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.firstjoinmessage.events.FirstSpawnEvent;
 import com.natamus.firstjoinmessage.util.Reference;
 import net.fabricmc.api.ModInitializer;
@@ -12,6 +13,10 @@ public class ModFabric implements ModInitializer {
 	
 	@Override
 	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		setGlobalConstants();
 		ModCommon.init();
 
--- Forge/src/main/java/com/natamus/firstjoinmessage/ModForge.java
+++ Forge/src/main/java/com/natamus/firstjoinmessage/ModForge.java
@@ -1,6 +1,7 @@
 package com.natamus.firstjoinmessage;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.firstjoinmessage.forge.config.IntegrateForgeConfig;
 import com.natamus.firstjoinmessage.forge.events.ForgeFirstSpawnEvent;
 import com.natamus.firstjoinmessage.util.Reference;
@@ -15,6 +16,10 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModForge {
 	
 	public ModForge() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 		modEventBus.addListener(this::loadComplete);
 
