fb58b93a8c0e20f1998c98babeb63634ac4eb235
Rick South <rick@natamus.com>
2024-12-06 13:32:01 +0100

    First Join Message source code for 1.21.4.

---
+++ Common/src/main/java/com/natamus/firstjoinmessage/events/FirstSpawnEvent.java
@@ -0,0 +1,45 @@
+package com.natamus.firstjoinmessage.events;
+
+import com.natamus.collective.functions.MessageFunctions;
+import com.natamus.collective.functions.PlayerFunctions;
+import com.natamus.collective.functions.MessageFunctions;
+import com.natamus.firstjoinmessage.config.ConfigHandler;
+import com.natamus.firstjoinmessage.util.Reference;
+
+import net.minecraft.ChatFormatting;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+
+public class FirstSpawnEvent {
+	public static void onSpawn(Level level, Entity entity) {
+		if (level.isClientSide) {
+			return;
+		}
+		
+		if (!(entity instanceof Player)) {
+			return;
+		}
+		
+		Player player = (Player)entity;
+		if (PlayerFunctions.isJoiningWorldForTheFirstTime(player, Reference.MOD_ID)) {
+			if (!ConfigHandler.firstJoinMessage.equals("")) {
+				ChatFormatting colour = ChatFormatting.getById(ConfigHandler.firstJoinMessageTextFormattingColourIndex);
+				if (colour == null) {
+					return;
+				}
+
+				MessageFunctions.sendMessage(player, ConfigHandler.firstJoinMessage, colour);
+			}
+
+			if (!ConfigHandler.serverBroadcastMessage.equals("")) {
+				ChatFormatting colour = ChatFormatting.getById(ConfigHandler.serverBroadcastMessageTextFormattingColourIndex);
+				if (colour == null) {
+					return;
+				}
+
+				MessageFunctions.broadcastMessage(level, ConfigHandler.serverBroadcastMessage, colour);
+			}
+		}
+	}
+}
