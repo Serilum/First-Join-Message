e2e0f5ad79af50ba1a0bd2edc82099b1e14e70bb
Rick South <rick@natamus.com>
2025-02-12 23:33:49 +0100

    (v3.7) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/firstjoinmessage/forge/events/ForgeFirstSpawnEvent.java
+++ Forge/src/main/java/com/natamus/firstjoinmessage/forge/events/ForgeFirstSpawnEvent.java
@@ -3,12 +3,10 @@ package com.natamus.firstjoinmessage.forge.events;
 import com.natamus.firstjoinmessage.events.FirstSpawnEvent;
 import net.minecraftforge.event.entity.EntityJoinLevelEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
-@EventBusSubscriber
 public class ForgeFirstSpawnEvent {
 	@SubscribeEvent
-	public void onSpawn(EntityJoinLevelEvent e) {
+	public static void onSpawn(EntityJoinLevelEvent e) {
 		FirstSpawnEvent.onSpawn(e.getLevel(), e.getEntity());
 	}
 }
