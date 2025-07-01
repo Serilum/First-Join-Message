3a13705d744440cbf505986b593f088f62ddbec5
Rick South <rick@natamus.com>
2025-07-01 10:18:24 +0200

    First Join Message source code for 1.21.7.

---
+++ Common/src/main/java/com/natamus/firstjoinmessage/events/FirstSpawnEvent.java
@@ -0,0 +1,42 @@
+package com.natamus.firstjoinmessage.events;
+
+import com.natamus.collective.functions.MessageFunctions;
+import com.natamus.collective.functions.PlayerFunctions;
+import com.natamus.firstjoinmessage.config.ConfigHandler;
+import com.natamus.firstjoinmessage.util.Reference;
+import net.minecraft.ChatFormatting;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+
+public class FirstSpawnEvent {
+	public static void onSpawn(Level level, Entity entity) {
+		if (level.isClientSide) {
+			return;
+		}
+		
+		if (!(entity instanceof Player player)) {
+			return;
+		}
+
+        if (PlayerFunctions.isJoiningWorldForTheFirstTime(player, Reference.MOD_ID)) {
+			if (!ConfigHandler.firstJoinMessage.isEmpty()) {
+				ChatFormatting colour = ChatFormatting.getById(ConfigHandler.firstJoinMessageTextFormattingColourIndex);
+				if (colour == null) {
+					return;
+				}
+
+				MessageFunctions.sendMessage(player, ConfigHandler.firstJoinMessage.replace("%s", player.getName().getString()), colour);
+			}
+
+			if (!ConfigHandler.serverBroadcastMessage.isEmpty()) {
+				ChatFormatting colour = ChatFormatting.getById(ConfigHandler.serverBroadcastMessageTextFormattingColourIndex);
+				if (colour == null) {
+					return;
+				}
+
+				MessageFunctions.broadcastMessage(level, ConfigHandler.serverBroadcastMessage.replace("%s", player.getName().getString()), colour);
+			}
+		}
+	}
+}
---
+++ NeoForge/src/main/java/com/natamus/firstjoinmessage/neoforge/events/NeoForgeFirstSpawnEvent.java
@@ -0,0 +1,12 @@
+package com.natamus.firstjoinmessage.neoforge.events;
+
+import com.natamus.firstjoinmessage.events.FirstSpawnEvent;
+import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+
+public class NeoForgeFirstSpawnEvent {
+	@SubscribeEvent
+	public static void onSpawn(EntityJoinLevelEvent e) {
+		FirstSpawnEvent.onSpawn(e.getLevel(), e.getEntity());
+	}
+}
